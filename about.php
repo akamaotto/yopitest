7ce2172e488088e64ad9048639d6d4427cebae97
akamaotto <akamaotto@gmail.com>
2014-11-10 07:47:39 +0100

    created an about file

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php
+echo "<html>";
+echo "<head>";
+echo "<title>";
+echo "About Us";
+echo "</title>";
+echo "</head>";
+echo "<body>";
+echo "About Content goes here";
+echo "</body>";
+echo "</html>";
+?>
